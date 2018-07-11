4cbd4427d46059e85fa2e8afd91405dbadc2ca2d
pramil <ppaudel35@gmail.com>
2018-07-11 22:48:40 +0545

    JSON body request added.

--- nube-rule-engine/src/main/java/io/nubespark/controller/RulesController.java
+++ nube-rule-engine/src/main/java/io/nubespark/controller/RulesController.java
@@ -78,37 +78,38 @@ public class RulesController {
     }
 
     public void getFiloData(RoutingContext routingContext) {
-        HttpServerRequest request = routingContext.request();
-        String query = request.getParam("query");
-        System.out.println("This is params" + query);
-        JsonObject queryObj = new JsonObject();
+        routingContext.request().bodyHandler((body) -> {
+            JsonObject jsonObject = new JsonObject(body);
+            String query = jsonObject.getString("query");
+            JsonObject queryObj = new JsonObject();
 //        Boolean isReadQuery = SQLFilter.isReadType(query);
 //        if (isReadQuery) {
             queryObj.put("query", query);
 //            queryObj.put("params", new JsonArray(Collections.singletonList("m:")));
 //        }
-        vertx.eventBus().send("io.nubespark.jdbc.engine", queryObj, message -> {
-            JsonObject replyJson = new JsonObject()
-                    .put("controller", "rules")
-                    .put("action", "getFiloData")
-                    .put("desc", "Read data from filodb")
-                    .put("query", query);
+            vertx.eventBus().send("io.nubespark.jdbc.engine", queryObj, message -> {
+                JsonObject replyJson = new JsonObject()
+                        .put("controller", "rules")
+                        .put("action", "getFiloData")
+                        .put("desc", "Read data from filodb")
+                        .put("query", query);
 //            if (!isReadQuery) {
 //                replyJson.put("access", Constants.ACCESS_DENIED);
 //            }
-            if (message.succeeded()) {
-                Object reply = message.result().body();
-                if (reply != null) {
-                    replyJson.put("resultSet", reply);
+                if (message.succeeded()) {
+                    Object reply = message.result().body();
+                    if (reply != null) {
+                        replyJson.put("resultSet", reply);
+                    }
+                } else {
+                    message.cause().printStackTrace();
+                    System.out.println(message.cause().getLocalizedMessage());
+                    System.out.println("Failed to receive reply...");
                 }
-            } else {
-                message.cause().printStackTrace();
-                System.out.println(message.cause().getLocalizedMessage());
-                System.out.println("Failed to receive reply...");
-            }
-            routingContext.response()
-                    .putHeader(CONTENT_TYPE, CONTENT_TYPE_JSON)
-                    .end(Json.encodePrettily(replyJson));
+                routingContext.response()
+                        .putHeader(CONTENT_TYPE, CONTENT_TYPE_JSON)
+                        .end(Json.encodePrettily(replyJson));
+            });
         });
 
     }
