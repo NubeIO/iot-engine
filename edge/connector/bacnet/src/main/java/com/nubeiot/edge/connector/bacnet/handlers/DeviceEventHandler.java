4a0f8acdc83d4dc751a2bd1432b62821dc69ba86
Shiny380 <daniel.mckinnell43@gmail.com>
2019-01-25 13:31:16 +1100

    BACNet implementation

---
+++ edge/connector/bacnet/src/main/java/com/nubeiot/edge/connector/bacnet/handlers/DeviceEventHandler.java
@@ -0,0 +1,43 @@
+package com.nubeiot.edge.connector.bacnet.handlers;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import io.vertx.core.json.JsonObject;
+import io.vertx.reactivex.core.Vertx;
+
+import com.nubeiot.core.dto.RequestData;
+import com.nubeiot.core.event.EventAction;
+import com.nubeiot.core.event.EventContractor;
+import com.nubeiot.core.event.EventHandler;
+import com.nubeiot.core.event.EventModel;
+import com.nubeiot.edge.connector.bacnet.BACnet;
+
+import lombok.Getter;
+
+/*
+ * VERTX event bus message handler
+ *  calls respective messages in BACnet
+ */
+
+
+public class DeviceEventHandler implements EventHandler {
+
+    private BACnet bacnetInstance;
+    private final Vertx vertx;
+    @Getter
+    private final List<EventAction> availableEvents;
+
+    public DeviceEventHandler(Vertx vertx, BACnet bacnetInstance, EventModel eventModel) {
+        this.vertx = vertx;
+        this.bacnetInstance = bacnetInstance;
+        this.availableEvents = Collections.unmodifiableList(new ArrayList<>(eventModel.getEvents()));
+    }
+
+    @EventContractor(action = EventAction.GET_LIST, returnType = JsonObject.class)
+    public JsonObject getList(RequestData data) {
+        return JsonObject.mapFrom(bacnetInstance.getRemoteDevices());
+    }
+
+}
