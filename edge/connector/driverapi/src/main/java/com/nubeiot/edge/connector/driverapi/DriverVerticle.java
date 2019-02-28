c6885f5c483769471f3b5326de9a742aa7c521f6
Shiny380 <daniel.mckinnell43@gmail.com>
2019-02-28 17:36:08 +1100

    Initial Commit. standalone http server with one route

--- edge/connector/driverapi/src/main/java/com/nubeiot/edge/connector/driverapi/DriverVerticle.java
+++ edge/connector/driverapi/src/main/java/com/nubeiot/edge/connector/driverapi/DriverVerticle.java
@@ -6,13 +6,8 @@ import com.nubeiot.core.component.ContainerVerticle;
 import com.nubeiot.core.event.EventController;
 import com.nubeiot.core.http.HttpServerProvider;
 import com.nubeiot.core.http.HttpServerRouter;
-import com.nubeiot.edge.connector.driverapi.handlers.DynamicEndpointsHandler;
-import com.nubeiot.edge.connector.driverapi.handlers.ServerEventHandler;
-import com.nubeiot.edge.connector.driverapi.models.DriverEventModels;
 
 public class DriverVerticle extends ContainerVerticle {
-
-    private EndpointsMapper endpointsMapper;
 
     @Override
     public void start() {
@@ -21,22 +16,16 @@ public class DriverVerticle extends ContainerVerticle {
         final NubeConfig nubeConfig = IConfig.from(config(), NubeConfig.class);
         logger.info("DriverAPI configuration: {}", this.nubeConfig.getAppConfig().toJson());
 
-        this.endpointsMapper = new EndpointsMapper();
-
         this.addProvider(new HttpServerProvider(initHttpRouter()));
         this.eventController = registerEventBus(new EventController(vertx));
     }
 
     private HttpServerRouter initHttpRouter() {
-        return new HttpServerRouter().registerEventBusApi(ServerRouteDefinitions.class);
+        return new HttpServerRouter().registerEventBusApi(DriverRouteDefinitions.class);
     }
 
     private EventController registerEventBus(EventController controller) {
-        controller.register(DriverEventModels.POINTS,
-                            new ServerEventHandler(vertx, DriverEventModels.POINTS, endpointsMapper));
-        controller.register(DriverEventModels.ENDPOINTS,
-                            new DynamicEndpointsHandler(vertx, DriverEventModels.ENDPOINTS, endpointsMapper));
-
+        controller.register(DriverEventModels.POINTS, new DriverEventHandler(vertx, DriverEventModels.POINTS));
         return controller;
     }
 
