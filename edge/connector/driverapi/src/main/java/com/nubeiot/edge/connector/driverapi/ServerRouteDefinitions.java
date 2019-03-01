1db372e297828fe12db4ca6e0a67cf6ecb684106
Shiny380 <daniel.mckinnell43@gmail.com>
2019-03-01 17:05:24 +1100

    Framework and data structure implemented

--- edge/connector/driverapi/src/main/java/com/nubeiot/edge/connector/driverapi/ServerRouteDefinitions.java
+++ edge/connector/driverapi/src/main/java/com/nubeiot/edge/connector/driverapi/ServerRouteDefinitions.java
@@ -7,8 +7,9 @@ import io.vertx.core.http.HttpMethod;
 import com.nubeiot.core.event.EventAction;
 import com.nubeiot.core.http.rest.AbstractRestEventApi;
 import com.nubeiot.core.http.rest.RestEventApi;
+import com.nubeiot.edge.connector.driverapi.models.DriverEventModels;
 
-public class DriverRouteDefinitions extends AbstractRestEventApi {
+public class ServerRouteDefinitions extends AbstractRestEventApi {
 
     @Override
     protected void initRoute() {
