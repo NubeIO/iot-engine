6764d1fe1243282c97b2ffc8f7650018a694c205
zero-88 <sontt246@gmail.com>
2019-03-04 21:49:08 +0700

    - Add sample project for BACNet and third party driver api

--- edge/connector/bacnet/src/main/java/com/nubeiot/edge/connector/bacnet/BACnetEventModels.java
+++ edge/connector/bacnet/src/main/java/com/nubeiot/edge/connector/bacnet/BACnetEventModels.java
@@ -11,16 +11,16 @@ public final class BACnetEventModels {
     public static final EventModel DEVICES = EventModel.builder()
                                                        .address("nubeiot.edge.connector.bacnet.device")
                                                        .pattern(EventPattern.REQUEST_RESPONSE)
+                                                       .local(true)
                                                        .events(Arrays.asList(EventAction.GET_LIST, EventAction.GET_ONE,
                                                                              EventAction.CREATE, EventAction.REMOVE))
-                                                       //                                                       .local(true)
                                                        .build();
 
     public static final EventModel POINTS = EventModel.builder()
                                                       .address("nubeiot.edge.connector.bacnet.device.points")
                                                       .pattern(EventPattern.REQUEST_RESPONSE)
-                                                      .events(Arrays.asList(EventAction.GET_LIST))
-                                                      //                                                      .local(true)
+                                                      .event(EventAction.GET_LIST)
+                                                      .local(true)
                                                       .build();
 
 }
