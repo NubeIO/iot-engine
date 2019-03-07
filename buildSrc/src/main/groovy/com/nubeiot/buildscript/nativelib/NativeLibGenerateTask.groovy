b8a2083afbbc84f1e51c9ca292af180a86d27522
RaiBnod <binodrayee@gmail.com>
2019-03-07 09:16:44 +0545

    - Extra unnecessary code removed out.

--- buildSrc/src/main/groovy/com/nubeiot/buildscript/nativelib/NativeLibGenerateTask.groovy
+++ buildSrc/src/main/groovy/com/nubeiot/buildscript/nativelib/NativeLibGenerateTask.groovy
@@ -45,7 +45,6 @@ class NativeLibsGenerateTask extends DefaultTask {
 
                 } else if (os.startsWith("win")) {
                     def dynamicLibName = "${file.toString().tokenize("/").last().replace(extension, "")}.dll"
-                    "x86_64-w64-mingw32-g++ -I\"%JAVA_HOME%\\include\" -I\"%JAVA_HOME%\\include\\win32\" -shared -o hello.dll HelloJNICpp.c HelloJNICppImpl.cpp"
 
                     proc = """x86_64-w64-mingw32-g++ -std=c++0x -I$jniIncludeDir -I$jniIncludeDir/linux -I$systemIncludeDir 
 -I$localIncludeDir -dynamiclib -o $nativeLibsSrcFolder/$dynamicLibName $file"""
