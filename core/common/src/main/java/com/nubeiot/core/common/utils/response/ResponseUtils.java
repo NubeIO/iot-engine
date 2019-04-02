1bd934048895a2c7cdabbba0ae7439c1bafab8e1
RaiBnod <binodrayee@gmail.com>
2019-04-02 23:17:32 +0545

    - Removed out migrated files and :core:common module.

--- core/common/src/main/java/com/nubeiot/core/common/utils/response/ResponseUtils.java
+++
@@ -1,9 +0,0 @@
-package com.nubeiot.core.common.utils.response;
-
-/**
- * Created by topsykretts on 4/26/18.
- */
-public class ResponseUtils {
-    public static String CONTENT_TYPE = "content-type";
-    public static String CONTENT_TYPE_JSON = "application/json; charset=utf-8";
-}
