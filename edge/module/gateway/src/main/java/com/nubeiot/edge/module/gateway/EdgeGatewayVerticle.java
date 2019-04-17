1f8fd15c9115a3e010d9cf58f4f0209989cbe612
RaiBnod <binodrayee@gmail.com>
2019-04-17 14:23:11 +0545

    - Removing info API and added serviceDescriptor.

--- edge/module/gateway/src/main/java/com/nubeiot/edge/module/gateway/EdgeGatewayVerticle.java
+++ edge/module/gateway/src/main/java/com/nubeiot/edge/module/gateway/EdgeGatewayVerticle.java
@@ -1,7 +1,6 @@
 package com.nubeiot.edge.module.gateway;
 
 import com.nubeiot.core.component.ContainerVerticle;
-import com.nubeiot.core.event.EventController;
 import com.nubeiot.core.http.HttpServerProvider;
 import com.nubeiot.core.http.HttpServerRouter;
 import com.nubeiot.core.micro.MicroserviceProvider;
@@ -11,14 +10,9 @@ public class EdgeGatewayVerticle extends ContainerVerticle {
     @Override
     public void start() {
         super.start();
-        HttpServerRouter router = new HttpServerRouter().registerEventBusApi(EdgeGatewayRestEventApi.class);
+        HttpServerRouter router = new HttpServerRouter();
         this.addProvider(new HttpServerProvider(router))
             .addProvider(new MicroserviceProvider());
-    }
-
-    @Override
-    public void registerEventbus(EventController controller) {
-        controller.register(EdgeGatewayEventModel.infoModel, new EdgeGatewayEventHandler());
     }
 
 }
