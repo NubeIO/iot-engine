7cfd33bc7a6576cb2d436193514232552b10d8b1
Nguyen Dang Thanh <thanhx0kjt@hotmail.com>
2019-05-03 01:45:13 +0700

    Implement bios startup modules and adding unit test

--- edge/bios/src/test/java/com/nubeiot/edge/bios/GettingStartupModulesTest.java
+++ edge/bios/src/test/java/com/nubeiot/edge/bios/GettingStartupModulesTest.java
@@ -7,7 +7,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
-
 import org.slf4j.LoggerFactory;
 
 import io.vertx.core.DeploymentOptions;
@@ -15,17 +14,21 @@ import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
 import io.vertx.reactivex.core.Vertx;
-import ch.qos.logback.classic.Level;
-import ch.qos.logback.classic.Logger;
-import lombok.NonNull;
 
 import com.nubeiot.core.NubeConfig;
 import com.nubeiot.core.NubeConfig.AppConfig;
 import com.nubeiot.core.NubeConfig.DeployConfig;
 import com.nubeiot.core.TestHelper;
+import com.nubeiot.core.enums.State;
 import com.nubeiot.core.sql.SqlConfig;
 import com.nubeiot.core.statemachine.StateMachine;
 import com.nubeiot.edge.core.EdgeVerticle;
+import com.nubeiot.edge.core.model.tables.daos.TblModuleDao;
+import com.nubeiot.edge.core.model.tables.pojos.TblModule;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import lombok.NonNull;
 
 @RunWith(VertxUnitRunner.class)
 public class GettingStartupModulesTest {
@@ -33,13 +36,13 @@ public class GettingStartupModulesTest {
     @Rule
     public TemporaryFolder folder = new TemporaryFolder();
     protected Vertx vertx;
-    protected EdgeVerticle initDataVerticle = new MockBiosVerticle(MockInitDataEntityHandler.class);
-    protected EdgeVerticle testVerticle = new MockBiosVerticle(MockEdgeEntityHandler.class);
+    protected EdgeVerticle testVerticle = new MockBiosVerticle(MockInitDataEntityHandler.class);
+    DeploymentOptions options;
 
     @BeforeClass
     public static void beforeSuite() {
         TestHelper.setup();
-        ((Logger) LoggerFactory.getLogger("com.nubeiot")).setLevel(Level.ERROR);
+        ((Logger) LoggerFactory.getLogger("com.nubeiot")).setLevel(Level.INFO);
         StateMachine.init();
     }
 
@@ -56,21 +59,15 @@ public class GettingStartupModulesTest {
         appConfig.put("__sql__", sqlConfig.toJson());
 
         nubeConfig.setAppConfig(appConfig);
-        DeploymentOptions options = new DeploymentOptions().setConfig(nubeConfig.toJson());
+        this.options = new DeploymentOptions().setConfig(nubeConfig.toJson());
         this.vertx = Vertx.vertx();
 
         Async async = context.async(1);
-        this.vertx.getDelegate().deployVerticle(this.initDataVerticle, options, context.asyncAssertSuccess(result -> {
+        this.vertx.getDelegate().deployVerticle(this.testVerticle, options, context.asyncAssertSuccess(result -> {
             TestHelper.testComplete(async);
         }));
-        async.awaitSuccess();
-        System.out.print("Starting verticle 2");
-        Async async1 = context.async(1);
-        this.vertx.getDelegate().deployVerticle(this.testVerticle, options, context.asyncAssertSuccess(result -> {
-            TestHelper.testComplete(async1);
-        }));
-        async1.awaitSuccess();
 
+        async.awaitSuccess();
     }
 
     public void after(TestContext context) {
@@ -82,7 +79,205 @@ public class GettingStartupModulesTest {
     }
 
     @Test
-    public void test_create_success(TestContext context) {
-        System.out.println(this.getJdbcUrl());
+    public void test_enabled_module(TestContext context) {
+        Async async1 = context.async(1);
+        long timer = this.vertx.setPeriodic(1000, event -> {
+            TblModuleDao moduleDao = this.testVerticle.getEntityHandler().getModuleDao();
+            assertModuleState(context, async1, moduleDao, State.ENABLED, "enabled-service");
+        });
+        this.vertx.setTimer(20000, event -> {
+            vertx.cancelTimer(timer);
+            context.fail("Testing failed");
+            TestHelper.testComplete(async1);
+        });
+        async1.awaitSuccess();
     }
+
+    @Test
+    public void test_pending_service_with_transaction_is_wip_prestate_action_is_init(TestContext context) {
+        Async async1 = context.async(1);
+        long timer = this.vertx.setPeriodic(1000, event -> {
+            TblModuleDao moduleDao = this.testVerticle.getEntityHandler().getModuleDao();
+            assertModuleState(context, async1, moduleDao, State.ENABLED,
+                              "pending-service-with-transaction-is-wip-prestate-action-is-init");
+        });
+        this.vertx.setTimer(20000, event -> {
+            vertx.cancelTimer(timer);
+            context.fail("Testing failed");
+            TestHelper.testComplete(async1);
+        });
+        async1.awaitSuccess();
+    }
+
+    @Test
+    public void test_pending_service_with_transaction_is_wip_prestate_action_is_create(TestContext context) {
+        Async async1 = context.async(1);
+        long timer = this.vertx.setPeriodic(1000, event -> {
+            TblModuleDao moduleDao = this.testVerticle.getEntityHandler().getModuleDao();
+            assertModuleState(context, async1, moduleDao, State.ENABLED,
+                              "pending-service-with-transaction-is-wip-prestate-action-is-create");
+        });
+        this.vertx.setTimer(20000, event -> {
+            vertx.cancelTimer(timer);
+            context.fail("Testing failed");
+            TestHelper.testComplete(async1);
+        });
+        async1.awaitSuccess();
+    }
+
+    @Test
+    public void test_pending_service_with_transaction_is_wip_prestate_action_is_update(TestContext context) {
+        Async async1 = context.async(1);
+        long timer = this.vertx.setPeriodic(1000, event -> {
+            TblModuleDao moduleDao = this.testVerticle.getEntityHandler().getModuleDao();
+            assertModuleState(context, async1, moduleDao, State.ENABLED,
+                              "pending-service-with-transaction-is-wip-prestate-action-is-update");
+        });
+        this.vertx.setTimer(20000, event -> {
+            vertx.cancelTimer(timer);
+            context.fail("Testing failed");
+            TestHelper.testComplete(async1);
+        });
+        async1.awaitSuccess();
+    }
+
+    @Test
+    public void test_pending_service_with_transaction_is_wip_prestate_action_is_patch(TestContext context) {
+        Async async1 = context.async(1);
+        long timer = this.vertx.setPeriodic(1000, event -> {
+            TblModuleDao moduleDao = this.testVerticle.getEntityHandler().getModuleDao();
+            assertModuleState(context, async1, moduleDao, State.ENABLED,
+                              "pending-service-with-transaction-is-wip-prestate-action-is-patch");
+        });
+        this.vertx.setTimer(20000, event -> {
+            vertx.cancelTimer(timer);
+            context.fail("Testing failed");
+            TestHelper.testComplete(async1);
+        });
+        async1.awaitSuccess();
+    }
+
+    @Test
+    public void test_pending_service_with_transaction_is_wip_prestate_action_is_update_disabled(TestContext context) {
+        Async async1 = context.async(1);
+        long timer = this.vertx.setPeriodic(1000, event -> {
+            TblModuleDao moduleDao = this.testVerticle.getEntityHandler().getModuleDao();
+            assertModuleState(context, async1, moduleDao, State.DISABLED,
+                              "pending-service-with-transaction-is-wip-prestate-action-is-update-disabled");
+        });
+        this.vertx.setTimer(20000, event -> {
+            vertx.cancelTimer(timer);
+            context.fail("Testing failed");
+            TestHelper.testComplete(async1);
+        });
+        async1.awaitSuccess();
+    }
+
+    @Test
+    public void test_pending_service_with_transaction_is_wip_prestate_action_is_patch_disabled(TestContext context) {
+        Async async1 = context.async(1);
+        long timer = this.vertx.setPeriodic(1000, event -> {
+            TblModuleDao moduleDao = this.testVerticle.getEntityHandler().getModuleDao();
+            assertModuleState(context, async1, moduleDao, State.DISABLED,
+                              "pending-service-with-transaction-is-wip-prestate-action-is-patch-disabled");
+        });
+        this.vertx.setTimer(20000, event -> {
+            vertx.cancelTimer(timer);
+            context.fail("Testing failed");
+            TestHelper.testComplete(async1);
+        });
+        async1.awaitSuccess();
+    }
+
+    @Test
+    public void test_disabled_module(TestContext context) {
+        Async async1 = context.async(1);
+        long timer = this.vertx.setPeriodic(1000, event -> {
+            TblModuleDao moduleDao = this.testVerticle.getEntityHandler().getModuleDao();
+            assertModuleState(context, async1, moduleDao, State.DISABLED, "disabled-module");
+        });
+        this.vertx.setTimer(20000, event -> {
+            vertx.cancelTimer(timer);
+            context.fail("Testing failed");
+            TestHelper.testComplete(async1);
+        });
+        async1.awaitSuccess();
+    }
+
+    @Test
+    public void test_module_with_2_transactions(TestContext context) {
+        Async async1 = context.async(1);
+        long timer = this.vertx.setPeriodic(1000, event -> {
+            TblModuleDao moduleDao = this.testVerticle.getEntityHandler().getModuleDao();
+            assertModuleState(context, async1, moduleDao, State.ENABLED, "pending_module_with_two_transactions");
+        });
+        this.vertx.setTimer(20000, event -> {
+            vertx.cancelTimer(timer);
+            context.fail("Testing failed");
+            TestHelper.testComplete(async1);
+        });
+        async1.awaitSuccess();
+    }
+
+    @Test
+    public void test_module_with_2_transactions_invalid(TestContext context) {
+        Async async1 = context.async(1);
+        long timer = this.vertx.setPeriodic(1000, event -> {
+            TblModuleDao moduleDao = this.testVerticle.getEntityHandler().getModuleDao();
+            assertModuleState(context, async1, moduleDao, State.DISABLED,
+                              "pending_module_with_two_transactions_invalid");
+        });
+        this.vertx.setTimer(20000, event -> {
+            vertx.cancelTimer(timer);
+            context.fail("Testing failed");
+            TestHelper.testComplete(async1);
+        });
+        async1.awaitSuccess();
+    }
+
+    @Test
+    public void test_pending_but_failed_module(TestContext context) {
+        Async async1 = context.async(1);
+        long timer = this.vertx.setPeriodic(1000, event -> {
+            TblModuleDao moduleDao = this.testVerticle.getEntityHandler().getModuleDao();
+            assertModuleState(context, async1, moduleDao, State.DISABLED, "pending-but-failed-module");
+        });
+        this.vertx.setTimer(20000, event -> {
+            vertx.cancelTimer(timer);
+            context.fail("Testing failed");
+            TestHelper.testComplete(async1);
+        });
+        async1.awaitSuccess();
+    }
+
+    @Test
+    public void test_getting_startup_modules(TestContext context) {
+        Async async = context.async(1);
+        this.testVerticle.getEntityHandler().getModulesWhenBootstrap().subscribe(modules -> {
+            System.out.println("Assert Size");
+            context.assertEquals(modules.size(), 6);
+            TestHelper.testComplete(async);
+        }, error -> {
+            context.fail();
+            TestHelper.testComplete(async);
+        });
+        async.awaitSuccess();
+    }
+
+    private void assertModuleState(TestContext context, Async async1, TblModuleDao moduleDao, State expectedState,
+                                   String moduleId) {
+        moduleDao.findOneById(moduleId).subscribe(result -> {
+            TblModule tblModule = result.orElse(null);
+            context.assertNotNull(tblModule);
+            if (tblModule.getState() != State.PENDING) {
+                System.out.println("Checking state of " + moduleId);
+                context.assertEquals(tblModule.getState(), expectedState);
+                TestHelper.testComplete(async1);
+            }
+        }, error -> {
+            context.fail(error);
+            TestHelper.testComplete(async1);
+        });
+    }
+
 }
