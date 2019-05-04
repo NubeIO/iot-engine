b40befd7f3c85bcde914ade867c8fe06cee9fc09
phadu02 <trungduc08061995@gmail.com>
2019-05-04 14:15:10 +0700

    add test when deploy failed

--- edge/bios/src/test/java/com/nubeiot/edge/bios/HandlerDeployFailedTest.java
+++ edge/bios/src/test/java/com/nubeiot/edge/bios/HandlerDeployFailedTest.java
@@ -14,7 +14,10 @@ import com.nubeiot.core.TestHelper;
 import com.nubeiot.core.enums.State;
 import com.nubeiot.core.enums.Status;
 import com.nubeiot.core.event.EventAction;
+import com.nubeiot.core.utils.DateTimes;
 import com.nubeiot.edge.core.EdgeVerticle;
+import com.nubeiot.edge.core.loader.ModuleType;
+import com.nubeiot.edge.core.model.tables.pojos.TblModule;
 
 @RunWith(VertxUnitRunner.class)
 public class HandlerDeployFailedTest extends BaseEdgeVerticleTest {
@@ -40,7 +43,7 @@ public class HandlerDeployFailedTest extends BaseEdgeVerticleTest {
     }
 
     @Test
-    public void test_deploy_failed(TestContext context) {
+    public void test_create_when_deploy_failed(TestContext context) {
         JsonObject appConfig = new JsonObject().put("", "");
         JsonObject metadata = new JsonObject().put("artifact_id", ARTIFACT_ID)
                                               .put("group_id", GROUP_ID)
@@ -53,6 +56,57 @@ public class HandlerDeployFailedTest extends BaseEdgeVerticleTest {
             async.awaitSuccess();
         });
         testingDBUpdated(context, State.DISABLED, Status.FAILED, appConfig);
+    }
+
+    @Test
+    public void test_update_when_deploy_failed(TestContext context) {
+        createService(context);
+
+        JsonObject appConfig = new JsonObject().put("", "");
+        JsonObject metadata = new JsonObject().put("state", State.ENABLED)
+                                              .put("version", VERSION)
+                                              .put("service_name", SERVICE_NAME);
+        JsonObject body = new JsonObject().put("service_id", MODULE_ID)
+                                          .put("metadata", metadata)
+                                          .put("appConfig", appConfig);
+        executeThenAssert(EventAction.UPDATE, context, body, (response, async) -> {
+            TestHelper.testComplete(async);
+            async.awaitSuccess();
+        });
+        testingDBUpdated(context, State.DISABLED, Status.FAILED, appConfig);
+    }
+
+    @Test
+    public void test_patch_when_deploy_failed(TestContext context) {
+        createService(context);
+        JsonObject metadata = new JsonObject().put("state", State.ENABLED).put("service_name", SERVICE_NAME);
+        JsonObject body = new JsonObject().put("service_id", MODULE_ID).put("metadata", metadata);
+        executeThenAssert(EventAction.PATCH, context, body, (response, async) -> {
+            TestHelper.testComplete(async);
+            async.awaitSuccess();
+        });
+        testingDBUpdated(context, State.DISABLED, Status.FAILED, DEPLOY_CONFIG);
+    }
+
+    @Test
+    public void test_delete_when_deploy_failed(TestContext context) {
+        createService(context);
+        JsonObject body = new JsonObject().put("service_id", MODULE_ID);
+        executeThenAssert(EventAction.REMOVE, context, body, (response, async) -> {
+            TestHelper.testComplete(async);
+            async.awaitSuccess();
+        });
+        testingDBUpdated(context, State.DISABLED, Status.FAILED, DEPLOY_CONFIG);
+    }
+
+    private void createService(TestContext context) {
+        this.insertModule(context, new TblModule().setServiceId(MODULE_ID)
+                                                  .setServiceType(ModuleType.JAVA)
+                                                  .setServiceName(SERVICE_NAME)
+                                                  .setState(State.ENABLED)
+                                                  .setVersion(VERSION)
+                                                  .setDeployConfig(DEPLOY_CONFIG)
+                                                  .setModifiedAt(DateTimes.nowUTC()));
     }
 
 }
