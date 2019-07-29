3e39bd763f0db7d8e628078c4ea3176007aa31cd
RaiBnod <binodrayee@gmail.com>
2019-07-29 10:06:39 +0545

    - Removed out the conditional query check in PostgreSQL (PostgreSQL user permission itself handles it).

--- dashboard/connector/postgresql/src/main/java/com/nubeiot/dashboard/connector/postgresql/controller/RulesController.java
+++ dashboard/connector/postgresql/src/main/java/com/nubeiot/dashboard/connector/postgresql/controller/RulesController.java
@@ -1,8 +1,5 @@
 package com.nubeiot.dashboard.connector.postgresql.controller;
 
-import com.nubeiot.core.common.utils.ErrorCodeException;
-import com.nubeiot.core.common.utils.ErrorCodes;
-
 import io.reactivex.Single;
 import io.vertx.core.json.JsonObject;
 import io.vertx.reactivex.core.Vertx;
@@ -15,18 +12,12 @@ public class RulesController {
     private final com.nubeiot.dashboard.connector.postgresql.reactivex.PostgreSQLService postgreSQLService;
 
     public RulesController(Vertx vertx) {
-        postgreSQLService = com.nubeiot.dashboard.connector.postgresql.PostgreSQLService.createProxy(vertx, com.nubeiot.dashboard.connector.postgresql.PostgreSQLService.SERVICE_ADDRESS);
+        postgreSQLService = com.nubeiot.dashboard.connector.postgresql.PostgreSQLService.createProxy(vertx,
+                                                                                                     com.nubeiot.dashboard.connector.postgresql.PostgreSQLService.SERVICE_ADDRESS);
     }
 
     public Single<JsonObject> getPostgreSQLData(String query, JsonObject settings) {
-        return Single.just(query).map(queryString -> {
-            // Only handleEvent select statements
-            if (queryString.toUpperCase().trim().startsWith("SELECT")) {
-                return true;
-            } else {
-                throw new ErrorCodeException(ErrorCodes.BAD_ACTION);
-            }
-        }).flatMap(ignored -> postgreSQLService.rxExecuteQuery(query, settings).map(message -> {
+        return Single.just(query).flatMap(ignored -> postgreSQLService.rxExecuteQuery(query, settings).map(message -> {
             JsonObject replyJson = new JsonObject().put("action", "PostgreSQL Data").put("query", query);
             if (message != null) {
                 replyJson.put("resultSet", message);
