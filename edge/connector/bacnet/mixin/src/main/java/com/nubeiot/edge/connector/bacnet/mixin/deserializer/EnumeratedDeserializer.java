777c4ac2a7b597b4ed56808b4e90a9e5eed2da94
zero-88 <sontt246@gmail.com>
2020-01-16 18:06:00 +0700

    BACnet - Fix BACnetDeviceTranslator for parsing system-status - Fix EnumeratedDeserializer to parse for Name Or Id - Fix NumberDeserializer to try parse string

--- edge/connector/bacnet/mixin/src/main/java/com/nubeiot/edge/connector/bacnet/mixin/deserializer/EnumeratedDeserializer.java
+++ edge/connector/bacnet/mixin/src/main/java/com/nubeiot/edge/connector/bacnet/mixin/deserializer/EnumeratedDeserializer.java
@@ -1,6 +1,11 @@
 package com.nubeiot.edge.connector.bacnet.mixin.deserializer;
 
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+import io.github.zero88.utils.Functions;
 import io.github.zero88.utils.Reflections.ReflectionMethod;
+import io.github.zero88.utils.Strings;
 
 import com.serotonin.bacnet4j.type.primitive.Enumerated;
 
@@ -24,8 +29,17 @@ final class EnumeratedDeserializer<T extends Enumerated> implements EncodableDes
     }
 
     @Override
+    public String cast(@NonNull Object value) {
+        return Strings.toString(value);
+    }
+
+    @Override
     public T parse(@NonNull String value) {
-        return ReflectionMethod.executeStatic(encodableClass(), "forName", value);
+        final Supplier<T> forName = () -> ReflectionMethod.executeStatic(encodableClass(), "forName", value);
+        final Function<Integer, T> forId = id -> ReflectionMethod.executeStatic(encodableClass(), "forId", id);
+        return Functions.getIfThrow(() -> Functions.toInt().apply(value))
+                        .map(id -> Functions.getOrDefault(() -> forId.apply(id), forName))
+                        .orElseGet(forName);
     }
 
 }
--- edge/connector/bacnet/src/test/java/com/nubeiot/edge/connector/bacnet/translator/BACnetDeviceTranslatorTest.java
+++ edge/connector/bacnet/src/test/java/com/nubeiot/edge/connector/bacnet/translator/BACnetDeviceTranslatorTest.java
@@ -29,42 +29,39 @@ import com.serotonin.bacnet4j.util.PropertyValues;
 public class BACnetDeviceTranslatorTest {
 
     private ObjectIdentifier oid;
-    private Address address;
     private JsonObject addressJson;
-    private PropertyValuesMixin mixin;
     private RemoteDeviceMixin deviceMixin;
-    private BACnetDeviceTranslator translator;
 
     @Before
     public void setup() {
-        translator = new BACnetDeviceTranslator();
         oid = new ObjectIdentifier(ObjectType.device, 444);
-        address = new Address(new byte[] {(byte) 206, (byte) 210, 100, (byte) 134});
+        final Address address = new Address(new byte[] {(byte) 206, (byte) 210, 100, (byte) 134});
         addressJson = new JsonObject(
             "{\"type\":\"IP\",\"networkNumber\":0,\"hostAddress\":\"206.210.100.134\",\"macAddress\":\"CE-D2-64-86\"}");
         final PropertyValues pvs = new PropertyValues();
         pvs.add(oid, PropertyIdentifier.vendorIdentifier, null, new UnsignedInteger(LocalDeviceMetadata.VENDOR_ID));
         pvs.add(oid, PropertyIdentifier.vendorName, null, new CharacterString(LocalDeviceMetadata.VENDOR_NAME));
-        mixin = PropertyValuesMixin.create(oid, pvs, false);
+        pvs.add(oid, PropertyIdentifier.systemStatus, null, DeviceStatus.operational);
+        final PropertyValuesMixin mixin = PropertyValuesMixin.create(oid, pvs, false);
         deviceMixin = RemoteDeviceMixin.create(oid, "test", address, mixin);
     }
 
     @Test
     public void test_serialize() throws JSONException {
-        final EdgeDeviceComposite composite = translator.serialize(deviceMixin);
+        final EdgeDeviceComposite composite = new BACnetDeviceTranslator().serialize(deviceMixin);
         JsonHelper.assertJson(addressJson, composite.getAddress());
         final Device device = composite.getDevice();
         Assert.assertEquals("device:444", device.getCode());
         Assert.assertEquals(DeviceType.EQUIPMENT, device.getType());
         Assert.assertEquals(Protocol.BACNET, device.getProtocol());
-        Assert.assertEquals(State.NONE, device.getState());
+        Assert.assertEquals(State.ENABLED, device.getState());
         Assert.assertEquals("1173-Nube iO Operations Pty Ltd", device.getManufacturer());
         JsonHelper.assertJson(deviceMixin.getPropertyValues().toJson(), device.getMetadata());
         JsonHelper.assertJson(deviceMixin.getAddress().toJson(), composite.getAddress());
     }
 
     @Test
-    public void test_deserialize() throws JSONException {
+    public void test_deserialize_without_metadata() throws JSONException {
         final Device device = new Device().setCode(ObjectIdentifierMixin.serialize(oid))
                                           .setManufacturer("1173-Nube iO Operations Pty Ltd")
                                           .setProtocol(Protocol.BACNET)
@@ -72,17 +69,22 @@ public class BACnetDeviceTranslatorTest {
                                           .setType(DeviceType.EQUIPMENT);
         final EdgeDeviceComposite composite = (EdgeDeviceComposite) new EdgeDeviceComposite().setDevice(device)
                                                                                              .setAddress(addressJson);
-        final RemoteDeviceMixin mixin = translator.deserialize(composite);
+        final RemoteDeviceMixin mixin = new BACnetDeviceTranslator().deserialize(composite);
         Assert.assertNotNull(mixin);
+        System.out.println(mixin.toJson());
         final PropertyValuesMixin propertyValues = mixin.getPropertyValues();
         JsonHelper.assertJson(mixin.getAddress().toJson(), addressJson);
         Assert.assertNull(mixin.getName());
         Assert.assertEquals(oid, mixin.getObjectId());
         Assert.assertEquals(444, mixin.getInstanceNumber());
         Assert.assertNotNull(propertyValues);
-        Assert.assertEquals(1173, (long) propertyValues.getAndCast(PropertyIdentifier.vendorIdentifier));
-        Assert.assertEquals("Nube iO Operations Pty Ltd", propertyValues.getAndCast(PropertyIdentifier.vendorName));
+        Assert.assertEquals(1173, (long) propertyValues.encode(PropertyIdentifier.vendorIdentifier));
+        Assert.assertEquals("Nube iO Operations Pty Ltd", propertyValues.encode(PropertyIdentifier.vendorName));
         Assert.assertEquals(DeviceStatus.operational, propertyValues.get(PropertyIdentifier.systemStatus));
     }
 
+    //    @Test
+    //    public void test_(){
+    //        final JsonObject entries = Configs.loadJsonConfig("device.json");
+    //    }
 }
