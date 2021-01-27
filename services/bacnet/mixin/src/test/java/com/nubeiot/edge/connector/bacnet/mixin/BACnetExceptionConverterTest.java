fcddf0a079f8b721161ada629dc4cd627ad754ce
zero88 <sontt246@gmail.com>
2021-01-27 20:40:21 +0700

    Add test for BACnet mixin

---
+++ services/bacnet/mixin/src/test/java/com/nubeiot/edge/connector/bacnet/mixin/BACnetExceptionConverterTest.java
@@ -0,0 +1,11 @@
+package com.nubeiot.edge.connector.bacnet.mixin;
+
+public class BACnetExceptionConverterTest {
+
+    //    @Test
+    //    public void convert() {
+    //        final NubeException convert = BACnetExceptionConverter.convert(
+    //            new AbortAPDUException(new Abort(true, (byte) 1, AbortReason.securityError)));
+    //        Assert.assertEquals("{\"data\":\"security-error\"}", convert.getMessage());
+    //    }
+}
